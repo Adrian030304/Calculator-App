6e56a64ef5f86d5c82fa01d7df96794832d73aad
Adrian030304 <adrianromanenco1@gmail.com>
2024-01-13 14:16:30 +0200

    made each operand clickable

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 let display = document.querySelector(".display");
 
-display.innerHTML = parseInt(0)
+display.innerHTML = ""
 
 let operands = document.getElementsByClassName("operand");
 console.log(operands)
-
+let operators = document.getElementsByClassName("operator")
+console.log(operators)
+const opArray = [...operands]
+/*
 for(let i = 0; i < operands.length; i++){
      operands[i].addEventListener("click", function() { 
           const clickedNumber = parseInt(operands[i].textContent);
           console.log(clickedNumber)
           display.innerHTML += clickedNumber;
      })
+     
 }
+*/
+opArray.forEach((operand) => {
+     console.log(operand.textContent)
+     operand.addEventListener("click",function(e){
+          const clickedNumber = parseInt(e.target.textContent)
+          display.textContent += clickedNumber;
+     })
+     
+});
+
+
+
 
 //functions for basic math operators
 const add = (a, b) => a + b;
