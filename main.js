b62d57fc56685d0a74b49dd11501f72a392fb3ac
Adrian030304 <adrianromanenco1@gmail.com>
2024-03-10 01:01:45 +0200

    changed small function details

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let display = document.querySelector(".display");
 
-display.innerHTML = ""
+display.textContent = ""
 
 let operands = document.getElementsByClassName("operand");
 console.log(operands)
 let operators = document.getElementsByClassName("operator")
 console.log(operators)
-const opArray = [...operands]
+const opArray = Array.from(operands)
 /*
 for(let i = 0; i < operands.length; i++){
      operands[i].addEventListener("click", function() { 
@@ -20,16 +20,15 @@ for(let i = 0; i < operands.length; i++){
 
 
 opArray.forEach((operand) => {
-     let numberInputted = operand.addEventListener("click",function(e){
-          const clickedNumber = parseInt(e.target.textContent)
-          display.textContent += clickedNumber;
-     })
-     console.log(numberInputted)
-});
+     operand.addEventListener("click", function(e) {
+         const clickedNumber = parseInt(e.target.textContent);
+         display.textContent += clickedNumber;
+     });
+ });
 
 
 
-
+/* 
 //functions for basic math operators
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
@@ -39,7 +38,7 @@ const divide = (a, b) => a / b;
 const num1 ;
 const operator;
 const num2;
-*/
-const operate = (num1,num2) => add(num1,num2);
 
+const operate = (num1,num2) => add(num1,num2);
+*/
 
