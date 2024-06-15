74552dd84246f683ac761454fc37936e17dd6816
Adrian030304 <adrianromanenco1@gmail.com>
2024-06-15 16:27:52 +0300

    little changes

--- main.js
+++ main.js
@@ -61,18 +61,3 @@ let selectedOperator = "";
      secondNumber = "";
      selectedOperator = "";
  });
-
-/* 
-//functions for basic math operators
-const add = (a, b) => a + b;
-const substract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-/*
-const num1 ;
-const operator;
-const num2;
-
-const operate = (num1,num2) => add(num1,num2);
-*/
-
