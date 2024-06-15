3214d6abf22c7f4ffec433ced93da62e6b595372
Adrian030304 <adrianromanenco1@gmail.com>
2024-06-15 17:11:53 +0300

    update.

--- main.js
+++ main.js
@@ -1,63 +1,18 @@
-const display = document.querySelector("#display");
+const display = document.getElementById("display");
 
-display.textContent = ""
+function appendToDisplay(input){
+    display.value += input
+}
 
-let operands = document.getElementsByClassName("operand");
-console.log(operands)
-let operators = document.getElementsByClassName("operator")
-console.log(operators)
-const operatorsArray = Array.from(operators)
-const opArray = Array.from(operands)
+function clearDisplay(){
+    display.value = ""
+}
 
-let firstNumber = "";
-let secondNumber = "";
-let selectedOperator = "";
-
-
- operatorsArray.forEach((operator) => {
-     operator.addEventListener("click", function(e) {
-         selectedOperator = e.target.textContent;
-         display.textContent += selectedOperator;
-
-
-         
-     });
- });
-//function to take in the number of each button
- opArray.forEach((operand) => {
-     operand.addEventListener("click", function(e) {
-        const pressedNumber = e.target.textContent;
-         if (selectedOperator === "") {
-          
-             firstNumber += pressedNumber;
-         } else {
-             secondNumber += pressedNumber;
-         }
-         display.textContent += pressedNumber;
-     });
- });
- 
- 
- document.querySelector("#equal").addEventListener("click", function() {
-     let result;
-     switch (selectedOperator) {
-         case "+":
-             result = parseInt(firstNumber) + parseInt(secondNumber);
-             break;
-         case "-":
-             result = parseInt(firstNumber) - parseInt(secondNumber);
-             break;
-         case "*":
-             result = parseInt(firstNumber) * parseInt(secondNumber);
-             break;
-         case "/":
-             result = parseInt(firstNumber) / parseInt(secondNumber);
-             break;
-         default:
-             result = "Invalid operator";
-     }
-     display.textContent = result !== "Invalid operator" ? result : "Error: Check Input";
-     firstNumber = "";
-     secondNumber = "";
-     selectedOperator = "";
- });
+function calculate(){
+    try{
+        display.value = eval(display.value)
+    } catch(error){
+        display.value = "Error"
+    }
+    
+}
