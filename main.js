836d3083c2b24eedd5c67e803bc7761a9b57a4cf
Adrian030304 <adrianromanenco1@gmail.com>
2024-06-17 00:59:59 +0300

    implemented the logic for the compute function

--- main.js
+++ main.js
@@ -1,78 +1,102 @@
-//created calculator class 
+//created calculator class
 class Calculator {
-    constructor(previousOperandTextElement,currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement;
-        this.currentOperandTextElement = currentOperandTextElement;
-        this.clear()
-    }
-    //add operations of the calculator
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+  //add operations of the calculator
 
-    clear(){ //clears the numbers
-        this.currentOperand = ''
-        this.previousOperand = ''
-        this.operation = undefined
+  clear() {
+    //clears the numbers
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {
+    //deletes a number
+  }
+  appendNumber(number) {
+    //appends the numbers to the screen
+    //this makes sure that the dot doen't get appened many time but once as intended by exiting the funtion
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+  chooseOperation(operation) {
+    //this condition is so the function doen't run a empty string
+    if (this.currentOperand === "") return;
+    //this computes the already inserted values when it's computed with another number as long as it's not empty
+    if (this.previousOperand !== "") {
+      this.compute();
     }
-    delete(){ //deletes a number 
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    //if a number is not imputed for computation then the function will be cancelled
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "/":
+        computation = prev / current;
+        break;
+       default:
+        return
     }
-    appendNumber(number){ //appends the numbers to the screen
-        //this makes sure that the dot doen't get appened many time but once as intended by exiting the funtion
-        if(number === '.' && this.currentOperand.includes('.')) return
-        this.currentOperand = this.currentOperand.toString() + number.toString()
-    }
-    chooseOperation(operation){
-        //this condition is so the function doen't run a empty string
-        if(this.currentOperand === '') return 
-        //this computes the already inserted values when it's computed with another number as long as it's not empty
-        if(this.previousOperand !== '') {
-            this.compute()
-        }
-        this.operation = operation
-        this.previousOperand = this.currentOperand
-        this.currentOperand = ''
-    }
+  }
 
-    compute(){
-        
-    }
-
-    updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
-    }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.previousOperandTextElement.innerText = this.previousOperand;
+  }
 }
 
-
-
-
 //initialized variables containing buttons
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
 
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
 
-const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-numberButtons.forEach(button=>{
-    button.addEventListener('click',()=>{
-        calculator.appendNumber(button.innerText)
-        calculator.updateDisplay()
-    })
-})
-
-operationButtons.forEach(button=>{
-    button.addEventListener('click',()=>{
-        calculator.chooseOperation(button.innerText)
-        calculator.updateDisplay()
-    })
-})
-
-equalsButton.addEventListener('click',button =>{
-    calculator.compute()
-    calculator.updateDisplay()
-})
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
